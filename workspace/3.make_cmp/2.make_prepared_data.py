ccefe5e207b8222dfee59ad451037c3ec24962aa
npuichigo <418121364@qq.com>
2017-10-09 16:29:16 +0800

    Code refactoring.

--- workspace/3.make_cmp/2.make_prepared_data.py
+++ workspace/3.make_cmp/2.make_prepared_data.py
@@ -18,6 +18,7 @@ import os
 import sys
 import struct
 import numpy as np
+import argparse
 from scipy import signal
 
 
@@ -61,7 +62,7 @@ def write_binary_file(data, output_file_name, with_dim=False):
     fid.close()
 
 
-if __name__ == '__main__':
+def main():
 
     if not os.path.exists('prepared_label'):
         os.mkdir('prepared_label')
@@ -72,9 +73,12 @@ if __name__ == '__main__':
         filename, _ = os.path.splitext(line.strip())
         print('processing ' + filename)
         sys.stdout.flush()
+
         label_mat = np.loadtxt(os.path.join('label', filename + '.lab'))
         cmp_mat = read_binary_file(
-            os.path.join('cmp', filename + '.cmp'), dimension=67)
+            os.path.join('cmp', filename + '.cmp'),
+            dimension=FLAGS.mgc_dim + FLAGS.lf0_dim + 1 + FLAGS.bap_dim)
+
         if label_mat.shape[0] <= cmp_mat.shape[0]:
             cmp_mat = cmp_mat[:label_mat.shape[0], :]
         else:
@@ -82,40 +86,76 @@ if __name__ == '__main__':
             rep = np.repeat(cmp_mat[-1:, :], frame_diff, axis=0)
             cmp_mat = np.concatenate([cmp_mat, rep], axis=0)
 
+        frame_num =label_mat.shape[0]
+
         write_binary_file(
             label_mat,
             os.path.join('prepared_label', filename + '.lab'))
 
-        write_binary_file(
-            cmp_mat,
-            os.path.join('prepared_cmp', filename + '.cmp'))
+        cmp_context_mat = np.zeros([
+            cmp_mat.shape[0], cmp_mat.shape[1] + 2 * FLAGS.f0_context], dtype=np.float32)
 
-        """
-        frame_num = min(label_mat.shape[0], cmp_mat.shape[0])
-        write_binary_file(
-            label_mat[:frame_num, :],
-            os.path.join('prepared_label', filename + '.lab'))
-        # f0 context
-        f0_context = 4
-        cmp_mat = cmp_mat[:frame_num, :]
-        cmp_context_mat = np.zeros([cmp_mat.shape[0], cmp_mat.shape[1] + 2 * f0_context], dtype=np.float32)
         # apply medfilt
-        #cmp_context_mat[:, : 60] = signal.medfilt(cmp_mat[:, : 60], kernel_size=(3, 1))
+        #cmp_context_mat[:, : FLAGS.mgc_dim] = signal.medfilt(
+        #    cmp_mat[:, : FLAGS.mgc_dim], kernel_size=(3, 1))
         # apply averfilt
-        cmp_context_mat[:, : 60] = signal.convolve2d(
-            cmp_mat[:, : 60], [[1.0 / 3], [1.0 / 3], [1.0 / 3]], mode='same', boundary='symm')
-        cmp_context_mat[:, 60] = cmp_mat[:, 60]
-        for i in xrange(-f0_context, f0_context + 1):
+        cmp_context_mat[:, : FLAGS.mgc_dim] = signal.convolve2d(
+            cmp_mat[:, : FLAGS.mgc_dim],
+            [[1.0 / 3], [1.0 / 3], [1.0 / 3]],
+            mode='same', boundary='symm')
+        cmp_context_mat[:, FLAGS.mgc_dim] = cmp_mat[:, FLAGS.mgc_dim]
+
+        for i in xrange(-FLAGS.f0_context, FLAGS.f0_context + 1):
             for j in xrange(frame_num):
                 index = j + i
                 if index < 0:
                     index = 0
                 elif index >= frame_num:
                     index = frame_num - 1
-                cmp_context_mat[j, 61 + i + f0_context] = cmp_mat[index, 61]
-        cmp_context_mat[:, 70:] = cmp_mat[:, 62:]
+                cmp_context_mat[j, FLAGS.mgc_dim + 1 + i + FLAGS.f0_context] = (
+                    cmp_mat[index, FLAGS.mgc_dim + 1])
+
+        cmp_context_mat[:, FLAGS.mgc_dim + 1 + 2 * (FLAGS.f0_context) + 1:] = (
+            cmp_mat[:, FLAGS.mgc_dim + 1 + FLAGS.lf0_dim:])
 
         write_binary_file(
-            cmp_context_mat[:frame_num, :],
+            cmp_context_mat,
             os.path.join('prepared_cmp', filename + '.cmp'))
-        """
+
+
+if __name__ == '__main__':
+    def _str_to_bool(s):
+        """Convert string to bool (in argparse context)."""
+        if s.lower() not in ['true', 'false']:
+            raise ValueError('Argument needs to be a '
+                             'boolean, got {}'.format(s))
+        return {'true': True, 'false': False}[s.lower()]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--mgc_dim',
+        type=int,
+        default=60,
+        help='The dimension of mgc.'
+    )
+    parser.add_argument(
+        '--lf0_dim',
+        type=int,
+        default=1,
+        help='The dimension of lf0.'
+    )
+    parser.add_argument(
+        '--bap_dim',
+        type=int,
+        default=5,
+        help='The dimension of bap.'
+    )
+    parser.add_argument(
+        '--f0_context',
+        type=int,
+        default=4,
+        help='The f0 context of output.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+
+    main()
